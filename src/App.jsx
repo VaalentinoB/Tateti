0593a10a46bf061070febff7a45ef609bb167584
VaalentinoB <valentinoburioni@hotmail.com>
2025-01-24 18:44:44 -0300

    updaates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const Turns = {
@@ -6,19 +6,37 @@ const Turns = {
   O: "O",
 };
 
-const square = ({ children, updateBoard, index }) => {
-  return <div className="square">{children} </div>;
+const Square = ({ children, index, updateBoard }) => {
+  return (
+    <div className="square" onClick={() => updateBoard(index)}>
+      {children}
+    </div>
+  );
 };
+
 function App() {
-  const board = Array(9).fill(null);
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(Turns.X);
+
+  const updateBoard = (index) => {
+    if (board[index]) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    setTurn(turn === Turns.X ? Turns.O : Turns.X);
+  };
 
   return (
     <main className="board">
       <h1>TaTeTi</h1>
       <section className="game">
-        {board.map((_, index) => {
-          return <square key={index} index={index}></square>;
-        })}
+        {board.map((value, index) => (
+          <Square key={index} index={index} updateBoard={updateBoard}>
+            {value}
+          </Square>
+        ))}
       </section>
       <p className="turn">Turno actual: {turn}</p>
     </main>
