7a90aad9ad63a6352785451281797892414bf60b
VaalentinoB <valentinoburioni@hotmail.com>
2025-01-24 18:37:30 -0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 
+const Turns = {
+  X: "X",
+  O: "O",
+};
+
+const square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children} </div>;
+};
 function App() {
-  return <h1>Hello world!</h1>;
+  const board = Array(9).fill(null);
+
+  return (
+    <main className="board">
+      <h1>TaTeTi</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return <square key={index} index={index}></square>;
+        })}
+      </section>
+      <p className="turn">Turno actual: {turn}</p>
+    </main>
+  );
 }
 
 export default App;
